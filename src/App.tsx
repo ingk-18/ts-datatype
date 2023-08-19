67a1470262b5404e12d17d5d007e45573e6ff535
ingk-18 <64448438+ingk-18@users.noreply.github.com>
2023-08-19 16:25:49 +0900

    型エイリアス

--- src/App.tsx
+++ src/App.tsx
@@ -2,59 +2,27 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  // 文字列
-  let str: string = "Hello";
-  str = "Bye";
-  console.log(str);
+  // 先頭大文字のパスカルケースを使う
+  type User = {
+    name: string;
+    age: number;
+  };
 
-  // 数値
-  let num: number = 102;
-  console.log(num);
+  const user: User = {
+    name: "Taro",
+    age: 20,
+  };
 
-  // 巨大な数値（nはBigIntを示すために付け加える必要がある）
-  let bignum: bigint = 103n;
-  console.log(bignum);
-
-  // 真偽値
-  let bool: boolean = true;
-  console.log(bool);
-
-  // null
-  let nullValue: null = null;
-  console.log(nullValue);
-
-  // undefined
-  let undefinedValue: undefined = undefined;
-  console.log(undefinedValue);
-
+  type UserName = string;
+  type UserAge = number;
   // リテラル型
-  // 型の中でも特定の値を定義するもの、数値・文字列、真偽値
+  type UserGender = "man" | "woman" | "others";
 
-  // 文字列リテラル型
-  type Fruit = "apple" | "orange" | "banana";
-  let fruit: Fruit;
-
-  fruit = "apple"; // OK
-  fruit = "orange"; // OK
-  // fruit = "grape"; // エラー：'grape'はFruit型に含まれていない値
-
-  // 数値リテラル型
-  type Age = 18 | 20 | 25;
-  let age: Age;
-
-  age = 18; // OK
-  age = 20; // OK
-  // age = 30; // エラー：30はAge型に含まれていない値
-
-  // 真偽値リテラル型
-  type Status = true | false;
-  let status: Status;
-
-  status = true; // OK
-  status = false; // OK
-  // status = 1; // エラー：1はStatus型に含まれていない値
-
-
+  const userProfile: UserProfile = {
+    name: "Ryo",
+    age: 21,
+    gender: "others",
+  };
 
   return <div className="App"></div>;
 }
