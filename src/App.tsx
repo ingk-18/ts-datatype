6c64ab172bc3afa24caf574cdaa2dd9fb2499966
ingk-18 <64448438+ingk-18@users.noreply.github.com>
2023-08-22 08:27:22 +0900

    Type Alias

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,38 @@ import "./App.css";
 
 function App() {
   // 先頭大文字のパスカルケースを使う
-  type User = {
+  type Name = string;
+  const user: Name = "Hoge太郎";
+
+  // 配列の型エイリアス
+  type Numbers = number[];
+
+  const numbers: Numbers = [1, 2, 3, 4, 5];
+
+  // 数値のリテラル型
+  type Age = 30; // 30という値のみを持つ型
+
+  // 文字列のリテラル型
+  type Gender = "male" | "female"; // "male" または "female" のみを持つ型
+
+  // 真偽値のリテラル型
+  type Status = true; // true という値のみを持つ型
+
+  // 複数のリテラル型を組み合わせたユニオン型
+  type Direction = "north" | "east" | "south" | "west"; // 指定された値のいずれかを持つ型
+
+  // オブジェクトの型エイリアス
+  type PersonData = {
     name: string;
     age: number;
+    hobbies: string[];
   };
 
-  const user: User = {
-    name: "Taro",
-    age: 20,
+  const person: PersonData = {
+    name: "Alice",
+    age: 30,
+    hobbies: ["reading", "hiking"],
   };
-
-  type UserName = string;
-  type UserAge = number;
-  // リテラル型
-  type UserGender = "man" | "woman" | "others";
-
-  type UserProfile = {
-    name: UserName;
-    age: UserAge;
-    gender: UserGender;
-  };
-
-  const userProfile: UserProfile = {
-    name: "Ryo",
-    age: 21,
-    gender: "others",
-  };
-
   return <div className="App"></div>;
 }
 
