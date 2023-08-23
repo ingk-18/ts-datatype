631ce1b83950959b1a57e281ddab2aa506d62bc0
ingk-18 <64448438+ingk-18@users.noreply.github.com>
2023-08-24 08:33:24 +0900

    色々な関数定義

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,37 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  function add(a: number, b: number): number {
-    return a + b;
+  // 基礎的な型定義
+  // function add(a: number, b: number): number {
+  //   return a + b;
+  // }
+
+  // const result = add(1, 2)
+
+  // const result = add(1, 1)
+
+  // 引数に違う型を入れた場合はエラーになる
+  // function add(a: number, b: number): number {
+  //   return a + b;
+  // }
+
+  // const result = add(1, 'hoge')
+
+  // 戻り値が無いパターン
+  function sayHello(name: string): void {
+    console.log(`Hello, ${name}!`);
   }
 
-  const result = add(1, 2)
+  // タイプエイリアスを使用する
+  type Math = (a: number, b: number) => number;
 
+  const add: Math = (a, b) => {
+    return a + b;
+  };
+
+  const result = add(1,4)
+
+  console.log(result);
   return <div className="App"></div>;
 }
 
