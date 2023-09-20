aa33d14de352a11567a5d03ad24089744e5d229c
ingk-18 <readog1224@yahoo.co.jp>
2023-09-21 07:52:56 +0900

    ジェネリクス関数

--- src/App.ts
+++ src/App.ts
@@ -1,24 +1,21 @@
 import React from "react";
 
 function App() {
-  // ジェネリクスを使用しない場合
-  // function echoString(value: string): string {
-  //   return value;
-  // }
+  // ジェネリクスを使用しない場合;
+  function echoString(value: string): string {
+    return value;
+  }
 
-  // function echoNumber(value: number): number {
-  //   return value;
-  // }
-
-  // const resultString = echoString("Hello, TypeScript!");
-  // const resultNumber = echoNumber(42);
+  function echoNumber(value: number): number {
+    return value;
+  }
 
   // ジェネリクスを使用する場合;
   function echo<T>(value: T): T {
     return value;
   }
 
-  // 型推論によりresultStringはstring型
+  // resultStringはstring型
   const resultString = echo<string>("Hello, TypeScript!");
 
   // 型推論によりresultNumberはnumber型になる
