a8394c604ecd9a06aa0b6a27af878922d4b499f2
ingk-18 <readog1224@yahoo.co.jp>
2023-09-23 10:12:29 +0900

    ジェネリクスクラス

--- src/App.ts
+++ src/App.ts
@@ -1,25 +1,51 @@
 import React from "react";
 
 function App() {
-  // ジェネリクスを使用しない場合;
-  function echoString(value: string): string {
-    return value;
+  // ジェネリクスを使用しない場合
+  class StringStorage {
+    private data: string[] = [];
+
+    addItem(item: string) {
+      this.data.push(item);
+    }
+
+    getItem(index: number): string {
+      return this.data[index];
+    }
   }
 
-  function echoNumber(value: number): number {
-    return value;
+  class NumberStorage {
+    private data: number[] = [];
+
+    addItem(item: number) {
+      this.data.push(item);
+    }
+
+    getItem(index: number): number {
+      return this.data[index];
+    }
   }
 
-  // ジェネリクスを使用する場合;
-  function echo<T>(value: T): T {
-    return value;
+  // ジェネリクスを使用する場合
+  class Storage<T> {
+    private data: T[] = [];
+
+    addItem(item: T) {
+      this.data.push(item);
+    }
+
+    getItem(index: number): T {
+      return this.data[index];
+    }
   }
 
-  // resultStringはstring型
-  const resultString = echo<string>("Hello, TypeScript!");
+  const stringStorage = new Storage<string>();
+  stringStorage.addItem("Hello");
+  const stringValue = stringStorage.getItem(0);
 
-  // 型推論によりresultNumberはnumber型になる
-  const resultNumber = echo(42);
+  const numberStorage = new Storage<number>();
+  numberStorage.addItem(42);
+  const numberValue = numberStorage.getItem(0);
 }
 
 export default App;
